37932af4ec5434cb3b719b46cdb7f51ec9d19cfc
Ebram Luis <ebramluis@outlook.com>
2019-10-30 14:50:05 +0200

    details old version

--- details.js
+++ details.js
@@ -6,16 +6,16 @@ homeBtn.addEventListener('mouseenter',function(){
     homeBtn.style.color="white";
 })
 homeBtn.addEventListener('mouseleave',function(){
-    homeBtn.style.color= "#3c3c3c";
+    homeBtn.style.color= "#f08080";
 })
 loginBtn.addEventListener('mouseenter',function(){
-    loginBtn.style.color="#3c3c3c";
+    loginBtn.style.color="#f08080";
 })
 loginBtn.addEventListener('mouseleave',function(){
     loginBtn.style.color= "white";
 })
 registerBtn.addEventListener('mouseenter',function(){
-    registerBtn.style.color="#3c3c3c";
+    registerBtn.style.color="#f08080";
 })
 registerBtn.addEventListener('mouseleave',function(){
     registerBtn.style.color= "white";
