d42434a1422322ea0534f7976974feba5530083d
Shenouda Fawzy <Shenouda.Fawzy@hotmail.com>
2019-10-31 21:10:25 +0200

    Fix Bug in Loading movies in Home page

--- home_script.js
+++ home_script.js
@@ -1,50 +1,48 @@
 
 // Creating A promise
 var moveis = "";
-var myData = "";
-let p = new Promise((resolve, reject) => {
-    var req = new XMLHttpRequest();
-    var myRes = "";
-    req.open("GET", "movies.json");
-    req.send("");
-    req.addEventListener('readystatechange', function () {
-        if (req.readyState == 4 && req.status == 200) {
-            myRes = req.response;
-            myData = JSON.parse(myRes);
-            moveis = myData;
-            resolve(myData);
-        } if (req.status == 404)
-            reject("Error While Fetching Data ");
+async function loadMoviesPromise(){
+    //var moveis = "";
+    var myData = "";
+    let p = new Promise((resolve, reject) => {
+        var req = new XMLHttpRequest();
+        var myRes = "";
+        req.open("GET", "movies.json");
+        req.send("");
+        req.addEventListener('readystatechange', function () {
+            if (req.readyState == 4 && req.status == 200) {
+                myRes = req.response;
+                myData = JSON.parse(myRes);
+                moveis = myData;
+                resolve(myData);
+            } if (req.status == 404)
+                reject("Error While Fetching Data ");
+        });
+    
     });
+    
+    
+    await p.then(data => {
+        console.log("hi");
+        moveis = data;
+        loadHomeWithMovies();
+    }).catch(error => {
+        console.log("Error");
+    });
+    //console.log("After fetching data");
+}
 
-});
+loadMoviesPromise();
 
-
-p.then(data => {
-    console.log("hi");
-    moveis = data;
-}).catch(error => {
-    console.log("Error");
-});
-
-
-window.addEventListener('load', function () {
-
+function loadHomeWithMovies(){
     var divs = document.querySelectorAll('#bodyTwoDivId');
     var imgs = document.querySelectorAll('img');
 
 
     var movieObj = { }
-    //console.log(moveis);
-    let ac = moveis["Action"];
-    let animationCat = moveis["Animation"];
-    let romanceCat = moveis["Romance"];
-    
-    // console.log(ac[0]);
     let j = 0;
     var m = 0;
     var catKeys = Object.keys(moveis);
-    // console.log(catKeys[0]);
     console.log(moveis[catKeys[0]]);
     for(let i = 0 ; i < imgs.length ; i++){
         j = i % catKeys.length;
@@ -56,5 +54,4 @@ window.addEventListener('load', function () {
         imgs[i].setAttribute('key',mvKey);
         imgs[i].src = imgSrc;
     }
-
-});
+}
